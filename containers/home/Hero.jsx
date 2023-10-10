1d1e78fc9367884d7686e58c263f026edad1bcd8
David Johnson <davemoses3@gmail.com>
2023-10-10 10:17:03 +0100

    align hero

--- containers/home/Hero.jsx
+++ containers/home/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
   return (
     <section className="gradient__bg">
       <div className="container mx-auto flex flex-col px-6 py-16 sm:px-0 lg:px-16 xl:px-5">
-        <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col items-center md:flex-row">
           <div className="flex flex-col md:mb-0 md:w-1/2 md:items-start md:pr-16 lg:flex-grow lg:pr-24 xl:my-16">
             <h1 className="mb-4 font-duplicate-san text-3xl font-medium text-gray-900 md:text-4xl xl:text-6xl">
               Access your school's future revenue today
